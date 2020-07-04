1b8632eeaf9072d1285338a9dcebb35ce2f100d5
juliancesaro <julianjcesaro@gmail.com>
2020-07-04 12:28:46 +1000

    Implemented react-scroll

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,50 @@
 import React from "react"
 import "./Navbar.css"
+import { Link, animateScroll as scroll } from "react-scroll"
 
 const Navbar = () => {
+  const scrollToTop = () => {
+    scroll.scrollToTop()
+  }
   return (
     <div className="navbar">
-      <p>HOME</p>
-      <p>ABOUT</p>
-      <p>PROJECTS</p>
-      <p>CONTACT</p>
+      <Link
+        activeClass="active"
+        to="home"
+        spy={true}
+        smooth={true}
+        duration={500}
+      >
+        HOME
+      </Link>
+      <Link
+        activeClass="active"
+        to="about"
+        spy={true}
+        smooth={true}
+        offset={1}
+        duration={500}
+      >
+        ABOUT
+      </Link>
+      <Link
+        activeClass="active"
+        to="projects"
+        spy={true}
+        smooth={true}
+        duration={500}
+      >
+        PROJECTS
+      </Link>
+      <Link
+        activeClass="active"
+        to="contact"
+        spy={true}
+        smooth={true}
+        duration={500}
+      >
+        CONTACT
+      </Link>
     </div>
   )
 }
