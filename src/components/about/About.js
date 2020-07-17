b4c7c7ad2b849012cc50a5a778c7507ac89e290c
juliancesaro <julianjcesaro@gmail.com>
2020-07-17 21:45:25 +1000

    Content and style changes

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,6 +3,7 @@ import "./About.css"
 import IsVisible from "react-is-visible"
 import Section from "../section/Section"
 import { Fade } from "react-reveal"
+import emoji from "react-easy-emoji"
 
 const About = () => {
   const softwareSkills = [
@@ -56,19 +57,40 @@ const About = () => {
               <h2>Who am I?</h2>
               <p>I'm Julian.</p>
               <p>
-                I'm a fourth-year software engineering student studying at
-                Macquarie University. I'm also an aspiring full-stack developer.
+                {emoji("⚡")} I'm a software engineering student at Macquarie
+                University.
+              </p>
+              <p>
+                {emoji("⚡")} I enjoy creating projects and learning new
+                technologies.
+              </p>
+              <p>
+                {emoji("⚡")} I'm imaginative, I'm a fast learner, and I love
+                solving problems.
               </p>
               <p>
                 I've always been passionate about technology, which is why I
-                decided to start studying software engineering in 2017. Since
-                then, I've learned a lot and hope to keep learning as
-                opportunities present themselves.
+                decided to start studying software engineering nearly four years
+                ago. Since then, I've learned a lot and aim to continue learning
+                by constantly challenging myself!
               </p>
-              <p>
-                I created this website for you to view some of my personal
-                projects and read a bit about myself. View away!
-              </p>
+              <div className="location-wrapper">
+                <svg
+                  className="octicon octicon-location"
+                  viewBox="0 0 16 16"
+                  version="1.1"
+                  width="16"
+                  height="16"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill="white"
+                    fillRule="evenodd"
+                    d="M11.536 3.464a5 5 0 010 7.072L8 14.07l-3.536-3.535a5 5 0 117.072-7.072v.001zm1.06 8.132a6.5 6.5 0 10-9.192 0l3.535 3.536a1.5 1.5 0 002.122 0l3.535-3.536zM8 9a2 2 0 100-4 2 2 0 000 4z"
+                  ></path>
+                </svg>
+                <p>Sydney, Australia</p>
+              </div>
             </div>
           </Fade>
           <Fade right duration={1000} distance="70px">
