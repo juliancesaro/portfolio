5f53b6fe97d2890cd184a0a1240bcaa37a13498b
juliancesaro <julianjcesaro@gmail.com>
2020-07-17 17:08:05 +1000

    Added Apollo request to Github GraphQL API

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,35 +1,88 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./Projects.css"
 import Project from "../project/Project"
-import data from "../../data/projects.json"
 import Section from "../section/Section"
 import { Fade } from "react-reveal"
+import ApolloClient, { gql } from "apollo-boost"
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  request: (operation) => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    })
+  },
+})
+const query = gql`
+  {
+    user(login: "juliancesaro") {
+      pinnedItems(first: 4, types: [REPOSITORY]) {
+        totalCount
+        edges {
+          node {
+            ... on Repository {
+              name
+              description
+              url
+              id
+              diskUsage
+              languages(first: 2, orderBy: { field: SIZE, direction: DESC }) {
+                nodes {
+                  name
+                  color
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 const Projects = () => {
-  const projects = data.projects
+  const [githubProjects, setGithubProjects] = useState([])
+  const [loadProjectsError, setLoadProjectsError] = useState(false)
 
-  return (
-    <section className="projects">
-      <Section title="Projects">
-        <div className="projects-content">
-          <ul className="projects-list">
-            {projects.map((project) => (
-              <li key={project.name}>
-                <Fade bottom duration={1000} distance="20px">
-                  <Project
-                    link={project.link}
-                    name={project.name}
-                    tags={project.tags}
-                    about={project.about}
-                  />
-                </Fade>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </Section>
-    </section>
-  )
+  useEffect(() => {
+    getProjects()
+  }, [])
+
+  const getProjects = async () => {
+    try {
+      const queryResult = await client.query({
+        query,
+      })
+      setGithubProjects(queryResult.data.user.pinnedItems.edges)
+    } catch (error) {
+      console.log(error)
+      setLoadProjectsError(true)
+    }
+  }
+
+  if (!loadProjectsError) {
+    return (
+      <section className="projects">
+        <Section title="Projects">
+          <div className="projects-content">
+            <ul className="projects-list">
+              {githubProjects.map((project) => (
+                <li key={project.node.name}>
+                  <Fade bottom duration={1000} distance="20px">
+                    <Project project={project.node} />
+                  </Fade>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Section>
+      </section>
+    )
+  } else {
+    return null
+  }
 }
 
 export default Projects
